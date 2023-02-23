40afa82f0bb9db17433c99f69b236705aa43bc79
RogerioMatos78 <rogerio.matos78@gmail.com>
2023-02-23 13:43:49 -0300

    char

---
+++ src/MergulhadorJAVA/ExemploChar.java
@@ -0,0 +1,11 @@
+package MergulhadorJAVA;
+
+public class ExemploChar {
+
+    public static void main(String[] args) {
+        char inicialNome = 'R';
+
+        System.out.println(inicialNome);
+    }
+
+}
