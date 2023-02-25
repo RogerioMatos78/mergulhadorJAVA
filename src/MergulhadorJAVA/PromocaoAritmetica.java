268de22383a58dec1118303770822fef344556cc
RogerioMatos78 <rogerio.matos78@gmail.com>
2023-02-24 22:33:32 -0300

    incompativeis g

--- src/MergulhadorJAVA/PromocaoAritmetica.java
+++ src/MergulhadorJAVA/PromocaoAritmetica.java
@@ -4,7 +4,7 @@ public class PromocaoAritmetica {
     public static void main(String[] args) {
         long x =10;
         long y = 5;
-        int z = x * y;
+        int z = (long) x * y;
 
         System.out.println(z);
     }
