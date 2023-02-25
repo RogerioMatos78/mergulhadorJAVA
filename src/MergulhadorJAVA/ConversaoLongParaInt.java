18a221e2d1f22b3d88e764c68c7fe4b5461ae7be
RogerioMatos78 <rogerio.matos78@gmail.com>
2023-02-24 22:14:42 -0300

    incompativeis

---
+++ src/MergulhadorJAVA/ConversaoLongParaInt.java
@@ -0,0 +1,14 @@
+package MergulhadorJAVA;
+
+public class ConversaoLongParaInt {
+    public static void main(String[] args) {
+        long x = 10;
+
+        int y = (int) x;
+
+        System.out.println("y "+y);
+        System.out.println("x "+x);
+
+
+    }
+}
---
+++ src/MergulhadorJAVA/PromocaoAritmetica.java
@@ -0,0 +1,11 @@
+package MergulhadorJAVA;
+
+public class PromocaoAritmetica {
+    public static void main(String[] args) {
+        long x =10;
+        long y = 5;
+        int z = x * y;
+
+        System.out.println(z);
+    }
+}
